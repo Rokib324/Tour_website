16e59d29b3c761af55ba958b7f84064760a26058
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-07-14 18:03:13 +0600

    implement nav with button

--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { TbAirBalloon } from 'react-icons/tb'
 
 const Navbar = () => {
   return (
-    <div className='bg-blue-800 transition-all duration-200 h-[12vh] z-[1000]'>
+    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
         <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
             {/* logo */}
             <div className='flex items-center space-x-2'>
@@ -15,16 +15,22 @@ const Navbar = () => {
                 <h1 className='text-xl md:text-2xl font-bold text-white uppercase'>Rokib's</h1>
             </div>
             {/* nav links */}
-            <div className='hidden md:flex items-center space-x-10'>
-                {navLinks.map((link)=>(
-                    <Link key={link.id} href={link.url}>
-                        <p className='relative text-white text-base font-medium w-fit block after:block after:content-[""] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition duration-300 after:origin-right'>{link.title}
-                        </p>
+            <div className="hidden md:flex items-center space-x-10">
+                {navLinks.map((link) => (
+                    <Link key={link.id} href={link.url} className="group relative">
+                    <span className="text-white text-lg font-medium">
+                        {link.title}
+                    </span>
+                    <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-yellow-300 transition-all duration-300 group-hover:w-full"></span>
                     </Link>
                 ))}
-            </div>
+                </div>
+                {/* Book Now button */}
+                <div className='hidden md:flex items-center space-x-4'>
+                    <button className='bg-white text-black px-8 py-2 rounded-md'>Book Now</button>
+                </div>
         </div>
-        </div>
+    </div>
   )
 }
 
